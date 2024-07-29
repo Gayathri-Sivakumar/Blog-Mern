6b14868eb8b3a3e05794ce9f5c502d6174e81908
Gayathri-Sivakumar <sivakumarg.19@uom.lk>
2024-07-29 11:50:10 +0530

    okay changed

--- Server/config/config.js
+++ Server/config/config.js
@@ -5,16 +5,14 @@ const assert = require("assert");
 
 dotenv.config();
 
-const { PORT, HOST_URL, HOST, MONGODB_URI } = process.env;
+const { PORT, CORS_ORIGIN, MONGODB_URI } = process.env;
 
 assert(PORT, "PORT is required");
-assert(HOST_URL, "HOST_URL is required");
-assert(HOST, "HOST is required");
+assert(CORS_ORIGIN, "CORS_ORIGIN is required");
 assert(MONGODB_URI, "MONGODB_URI is required");
 
 module.exports = {
   port: PORT,
-  url: HOST_URL,
-  host: HOST,
+  url: CORS_ORIGIN,
   mongodbUri: MONGODB_URI,
 };
