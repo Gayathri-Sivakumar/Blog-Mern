9dd7a8ee2f0b7db8f6a24e32aff631885b25ab8b
Gayathri-Sivakumar <sivakumarg.19@uom.lk>
2024-07-29 16:22:50 +0530

    use effect removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -22,17 +22,6 @@ import { Box } from "@mui/material";
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userRole, setUserRole] = useState("");
-
-  useEffect(() => {
-    // Retrieve authentication state from localStorage
-    const storedToken = localStorage.getItem("authToken");
-    const storedRole = localStorage.getItem("userRole");
-
-    if (storedToken) {
-      setIsLoggedIn(true);
-      setUserRole(storedRole || "");
-    }
-  }, []);
 
   const handleLogin = (role) => {
     setIsLoggedIn(true);
